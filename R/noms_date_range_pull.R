8ba8ae7c53cdc02f78aecac727d4954d011ef43e
Megan Hines <mhines@usgs.gov>
2021-06-01 14:35:30 -0500

    move function outside pull_GA_data so it doesn't pull unnecessary data

--- R/noms_date_range_pull.R
+++ R/noms_date_range_pull.R
@@ -9,16 +9,14 @@ yesterday <- Sys.Date()-1
 
 yesterday <- seq(from=yesterday, to=yesterday, by='days')
 
-source('R/pull_GA_data.R')
+source('R/functions.R')
+source('R/group_data.R')
 
 for ( i in seq_along(yesterday) ) {
   
   message(paste0("pulling data for: ", yesterday[i]))
   df <- get_nwis_site_page_views(date_range = c(yesterday[i], yesterday[i]))
   message(paste0("pulled ", nrow(df)," rows."))
-  
-  source('R/functions.R')
-  source('R/group_data.R')
   
   message(paste0("summarizing data"))
   df <- group_by_site_id(df)
