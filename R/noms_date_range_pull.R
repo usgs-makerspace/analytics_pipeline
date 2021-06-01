e13d3715ab5d691ebe35ca032eefef5deee14329
Megan Hines <mhines@usgs.gov>
2021-06-01 12:51:43 -0500

    adjust logic and drop redundancy

--- R/noms_date_range_pull.R
+++ R/noms_date_range_pull.R
@@ -3,31 +3,11 @@ library(googleAuthR)
 library(arrow)
 library(tidyr)
 library(lubridate)
-library(plyr)
 library(dplyr)
 
-yesterday <- seq(from=as.Date('2019-10-01'), to=as.Date('2020-09-30'), by='days')
+yesterday <- seq(from=as.Date('2021-05-20'), to=as.Date('2021-05-31'), by='days')
 
-get_nwis_site_page_views <- function(date_range = c('2020-01-01', '2020-02-01')) {
-  gar_auth_service('~/.vizlab/VIZLAB-a48f4107248c.json')
-  nwis_web_view <- '49785472'
-  
-  #filter 
-  #get all pagePaths with numeric data between 8 and 15 digits long in it:
-  filter_8_15_digit_site <- dim_filter("pagePath", "REGEXP", expressions = "[0-9]{8,15}")
-  #########
-  
-  ###########
-  filter_clause_site_page <- filter_clause_ga4(list(filter_8_15_digit_site),
-                                               operator = "AND")
-  
-  site_number_page_views <- google_analytics(nwis_web_view, date_range = date_range,
-                                      dimensions = c("pagePath", "date"), slow_fetch = TRUE,
-                                      metrics = c("uniquePageviews","pageViews"),
-                                      max = -1, dim_filters = filter_clause_site_page,
-                                      rows_per_call = 100000, anti_sample = TRUE)
-  
-}
+source('R/pull_GA_data.R')
 
 for ( i in seq_along(yesterday) ) {
   
